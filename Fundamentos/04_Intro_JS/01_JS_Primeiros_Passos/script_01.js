ca069b145eaf4e7f46e0d7c1e436fc4a9e077905
Ezequiel Machado <ezeca122@hotmail.com>
2022-03-07 19:13:26 -0300

    parte 2

--- Fundamentos/04_Intro_JS/01_JS_Primeiros_Passos/script_01.js
+++ Fundamentos/04_Intro_JS/01_JS_Primeiros_Passos/script_01.js
@@ -1,11 +1,25 @@
 //Parte 1
 
-const a = 5;
+/* const a = 5;
 const b = 10;
 
 console.log(a+b);
 console.log(a-b);
 console.log(a*b);
 console.log(a/b);
-console.log(a%b);
+console.log(a%b); */
 
+//Parte 2
+
+const num1 = 10;
+const num2 = 30;
+
+if (num1 > num2) {
+    console.log(num1);
+}
+else if (num1 == num2) {
+    console.log("São iguais");
+}
+else {
+    console.log(num2);
+}

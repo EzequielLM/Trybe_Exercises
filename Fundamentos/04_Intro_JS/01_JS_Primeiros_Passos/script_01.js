fc2a6d4704c79001decfb5341dddb22fb668787a
Ezequiel Machado <ezeca122@hotmail.com>
2022-03-07 19:17:24 -0300

    parte 3

--- Fundamentos/04_Intro_JS/01_JS_Primeiros_Passos/script_01.js
+++ Fundamentos/04_Intro_JS/01_JS_Primeiros_Passos/script_01.js
@@ -11,7 +11,7 @@ console.log(a%b); */
 
 //Parte 2
 
-const num1 = 10;
+/* const num1 = 10;
 const num2 = 30;
 
 if (num1 > num2) {
@@ -22,4 +22,21 @@ else if (num1 == num2) {
 }
 else {
     console.log(num2);
+}
+ */
+
+//Parte 3
+
+const num1 = 10;
+const num2 = 20;
+const num3 = 30;
+
+if (num1 > num2 && num1 > num3) {
+    console.log(num1);
+}
+else if (num2 > num1 && num2 > num3) {
+    console.log(num2);
+}
+else if (num3 > num1 && num3 > num2) {
+    console.log(num3);
 }

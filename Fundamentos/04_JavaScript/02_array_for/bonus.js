572af356bb31e71b733c6ae58521dde345a18815
Ezequiel Machado <ezeca122@hotmail.com>
2022-03-08 18:53:00 -0300

    bonus parte 2

--- Fundamentos/04_JavaScript/02_array_for/bonus.js
+++ Fundamentos/04_JavaScript/02_array_for/bonus.js
@@ -4,9 +4,23 @@ console.log(numbers);
 
 //Parte 1
 
-for (let index = 1; index < numbers.length; index += 1) {
+/* for (let index = 1; index < numbers.length; index += 1) {
     for (let index2 = 0; index2 < index; index2 += 1){
         if(numbers[index] < numbers[index2]){
+            let position = numbers[index];
+            numbers[index] = numbers[index2];
+            numbers[index2] = position;
+        }
+    }
+}
+
+console.log(numbers); */
+
+//Parte 2 
+
+for (let index = 1; index < numbers.length; index += 1) {
+    for (let index2 = 0; index2 < index; index2 += 1){
+        if(numbers[index] > numbers[index2]){
             let position = numbers[index];
             numbers[index] = numbers[index2];
             numbers[index2] = position;

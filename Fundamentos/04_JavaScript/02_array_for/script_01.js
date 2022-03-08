8b071aebef5b89a39be33237bc63e9a42d4b017d
Ezequiel Machado <ezeca122@hotmail.com>
2022-03-08 18:17:02 -0300

    parte 5

--- Fundamentos/04_JavaScript/02_array_for/script_01.js
+++ Fundamentos/04_JavaScript/02_array_for/script_01.js
@@ -1,5 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sumNumbers = 0;
+/* let sumNumbers = 0; */
 //Parte 1
 
 /* for (let i = 0; i < numbers.length; i += 1) {
@@ -8,24 +8,35 @@ let sumNumbers = 0;
 
 //Parte 2
 
-for (let i = 0; i < numbers.length; i += 1) {
+/* for (let i = 0; i < numbers.length; i += 1) {
     sumNumbers += numbers[i];
-}
+} */
 
 /* console.log(sumNumbers); */
 
 
 //Parte 3
 
-let media = sumNumbers / numbers.length;
+/* let media = sumNumbers / numbers.length;
 
-console.log(media);
+console.log(media); */
 
 //Parte 4
 
-if(media > 20) {
+/* if(media > 20) {
     console.log('Valor maior que 20');
 }
 else {
     console.log('Valor menor que 20');
+} */
+
+//Parte 5
+let maior = 0;
+
+for(let i = 0; i < numbers.length; i += 1) {
+    if(numbers[i] > maior){
+        maior = numbers[i];
+    }
 }
+
+console.log(maior);

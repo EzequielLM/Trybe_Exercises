f801b310c05dd19be5c01da381f043a4a66ad644
Ezequiel Machado <ezeca122@hotmail.com>
2022-03-08 18:03:21 -0300

    parte 2

--- Fundamentos/04_JavaScript/02_array_for/script_01.js
+++ Fundamentos/04_JavaScript/02_array_for/script_01.js
@@ -1,8 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
+let sumNumbers = 0;
 //Parte 1
 
-for (let i = 0; i < numbers.length; i += 1) {
+/* for (let i = 0; i < numbers.length; i += 1) {
     console.log(numbers[i]);
+} */
+
+//Parte 2
+
+for (let i = 0; i < numbers.length; i += 1) {
+    sumNumbers += numbers[i];
 }
 
+console.log(sumNumbers);

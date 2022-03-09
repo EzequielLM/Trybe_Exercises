1cb64c935a2dcf5bab1535af569fb3eb5bde0d75
Ezequiel Machado <ezeca122@hotmail.com>
2022-03-09 18:31:35 -0300

    Parte 1

---
+++ Fundamentos/04_JS/03_Lógica_algoritmo/fatorial.js
@@ -0,0 +1,13 @@
+// <=======Parte 1=======>
+
+//O fatorial é representado pelo sinal !
+//! = 4 x 3 x 2 x 1 = 24
+
+let fatorial = 1;
+
+for(let i = 10; i > 0; i -= 1){
+    fatorial *= i;
+}
+console.log(fatorial);
+
+// <=======Parte 2=======>

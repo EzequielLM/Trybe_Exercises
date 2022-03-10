f75b2e3d15c69401bdbf5eeada397858f00a1e96
Ezequiel Machado <ezeca122@hotmail.com>
2022-03-10 19:10:22 -0300

    parte 5

--- Fundamentos/04_JS/04_function_object_forIn_forOf/Objects_forIn_forOf.js
+++ Fundamentos/04_JS/04_function_object_forIn_forOf/Objects_forIn_forOf.js
@@ -16,10 +16,24 @@ let info = {
 
   for(let valor in info){
       console.log(valor);
-  }
+  } 
 
   //* Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
 
   for(let valor in info){
     console.log(info[valor]);
 }
+
+  //* Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+
+  let infoTioPatinhas = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+  };
+
+  for(let valor in info){
+    console.log(info[valor] + ' e ' + infoTioPatinhas[valor]);
+  }
+

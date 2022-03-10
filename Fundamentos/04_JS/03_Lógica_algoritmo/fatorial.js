8b8a7d51da83c0c9d17f976498d85edf5d5c2e9b
Ezequiel Machado <ezeca122@hotmail.com>
2022-03-10 13:30:19 -0300

    parte 4

--- Fundamentos/04_JS/03_Lógica_algoritmo/fatorial.js
+++ Fundamentos/04_JS/03_Lógica_algoritmo/fatorial.js
@@ -10,13 +10,57 @@ for(let i = 10; i > 0; i -= 1){
 }
 console.log(fatorial);
 
+//OutPut -> 3628800
+
 // <=======Parte 2=======>
 
 let word = 'tryber';
 let drow = '';
 
-for(i = word.length - 1; i >= 0; i -= 1){
+for(let i = word.length - 1; i >= 0; i -= 1){
     drow += word[i];
 }
 console.log(word);
 console.log(drow);
+
+//OutPut -> tryber | rebyrt
+
+// <=======Parte 3=======>
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior = array[0];
+let menor = array[0];
+
+for(let i = 0; i < array.length; i += 1){
+    if(array[i].length > maior.length){
+        maior = array[i];
+    }
+    else if(array[i].length < menor.length){
+        menor = array[i];
+    }
+}
+
+console.log(maior);
+console.log(menor);
+
+//OutPut -> javascript | css
+
+// <=======Parte 4=======>
+
+let primo = 0;
+
+for(let i1 = 1; i1 < 50; i1 += 1){
+    let count = 0;
+    for(let i2 = 1; i2 < 50; i2 += 1){
+        if(i1 % i2 === 0){
+            count += 1;
+    }
+    }
+    if(count === 2){
+        primo = i1;
+    }
+}
+
+console.log(primo);
+
+//OutPut -> 47

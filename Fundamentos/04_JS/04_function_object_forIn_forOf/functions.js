d3c24a1630f6532b1a75d21ed220df56126c4480
Ezequiel Machado <ezeca122@hotmail.com>
2022-03-15 18:48:30 -0300

    .

--- Fundamentos/04_JS/04_function_object_forIn_forOf/functions.js
+++ Fundamentos/04_JS/04_function_object_forIn_forOf/functions.js
@@ -38,8 +38,8 @@ console.log(indiceMaiorValor(arrayMaior));
 //! Criar uma função que reeceba um array de inteiros
 //! e retorne o índice do menor valor
 
-function indiceMaiorValor(array){
-    
+function indiceMenorValor(array){
+
     let count = 0;
 
     for(let i = 0; i < array.length; i += 1){
@@ -53,7 +53,7 @@ function indiceMaiorValor(array){
 
 let arrayMenor = [2, 4, 6, 7, 10, 0, -3];
 
-console.log(indiceMaiorValor(arrayMenor));
+console.log(indiceMenorValor(arrayMenor));
 
 //! Criar uma função que reeceba um array de nomes
 //! e retorne o que tiver mais caracteres 
@@ -68,6 +68,7 @@ function maisCaracteres(array){
         }
     }
 
+
     return maiorNome;
 }
 
@@ -75,18 +76,59 @@ let arrayNomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 
 console.log(maisCaracteres(arrayNomes));
 
-
 //! Criar uma função que reeceba um array de inteiros
 //! e retorne o inteiro que mais se repete
 
 function maisVezes(array){
-    let maisVezes = 0;
+    let maisVezes;
+    let index = 0;
 
-    
+    for(let i = 0; i < array.length; i += 1){
+        let count = 0;
+        let count2 = 0;
 
+        for(let j = 0; j < array.length; j += 1){
+            count2 = count;
+            if(array[i] === array[j]){
+                count += 1;
+            }
+            
+            if(count > count2){
+                index = array[i];
+            }
+            else{
+                index = array[j];
+            }
+        }
+    }
+    maisVezes = index;
     return maisVezes;
 }
 
 let arrayMaisVezes = [2, 3, 2, 5, 8, 2, 3];
 
 console.log(maisVezes(arrayMaisVezes));
+
+
+//! GABARITO
+
+/* function maisRepetido(numeros) {
+    let contRepetido = 0;
+    let contNumero = 0;
+    let indexNumeroRepetido = 0;
+    for (let index in numeros) {
+      let verificaNumero = numeros[index];
+      for (let index2 in numeros) {
+        if (verificaNumero === numeros[index2]) {
+          contNumero += 1;
+        }
+      }
+      if (contNumero > contRepetido) {
+        contRepetido = contNumero;
+        indexNumeroRepetido = index;
+      }
+      contNumero = 0;
+    }
+    return numeros[indexNumeroRepetido];
+  }
+ */

5735f9e0f6a9160941efc6bbd3202e3be8e475b7
Ezequiel Machado <ezeca122@hotmail.com>
2022-04-05 19:24:25 -0300

    exercicio 1.3 e 1.4

--- Fundamentos/07_ES6/03_Jest/sum.test.js
+++ Fundamentos/07_ES6/03_Jest/sum.test.js
@@ -2,15 +2,24 @@ describe('Função sum', () => {
 
 const sum = require('./sum');
 
+//Teste se o retorno de sum(4, 5) é 9
 test('Soma 4 e 5 = 9', () => {
     expect(sum(4, 5)).toBe(9);
 })
 
+//Teste se o retorno de sum(0, 0) é 0
 test('Soma 0 e 0 = 0', () => {
     expect(sum(0, 0)).toBe(0);
 })
 
-//test('Retorna erro quando há string', () => {
-//    expect(sum(4, "5")).toBe(Error);
-//})
+//Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+test('Retorna erro quando há string', () => {
+    expect(() => sum(4, "5")).toThrowError();
+})
+
+//Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+test('A string do erro deve ser parameters must be numbers', () => {
+    expect(() => sum(4, "5")).toThrowError('parameters must be numbers');
+})
 });
+
